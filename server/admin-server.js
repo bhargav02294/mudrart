fb095cf1c9f1c40fb1c8c26eb460d96160344261
bhargav02294 <bhargavahire22@gmail.com>
2025-10-02 20:59:41 +0530

    Fix admin code

--- server/admin-server.js
+++ server/admin-server.js
@@ -1,5 +1,6 @@
 const express = require("express");
-require("dotenv").config({ path: "E:/coin-art/.env" });
+require("dotenv").config(); // Let Render use its own env vars
+
 const path = require("path");
 const mongoose = require("mongoose");
 const cors = require("cors");
