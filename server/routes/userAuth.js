65fa9cab8a31479621ca623d16478856d2205d9f
bhargav02294 <bhargavahire22@gmail.com>
2026-02-21 19:11:42 +0530

    user setup and mail2

--- server/routes/userAuth.js
+++ server/routes/userAuth.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const User = require("../models/User");
-const sendMail = require("../utils/sendMail");
+const sendMail = require("../util/sendMail");
 
 const router = express.Router();
 
