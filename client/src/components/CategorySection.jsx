1ce6bdad150674e004247177537157ee06e15304
bhargav02294 <bhargavahire22@gmail.com>
2026-04-21 14:15:57 +0530

    category2

--- client/src/components/CategorySection.jsx
+++ client/src/components/CategorySection.jsx
@@ -1,13 +1,13 @@
 import "../styles/categories.css";
 
 const categories = [
-  { name: "Entertainment", icon: "/categories/entertainment.png", key: "entertainment" },
-  { name: "Superheroes", icon: "/categories/superheroes.png", key: "marvel_dc" },
-  { name: "Sports", icon: "/categories/sports.png", key: "sports" },
-  { name: "Anime", icon: "/categories/anime.png", key: "anime" },
-  { name: "Aesthetic", icon: "/categories/aesthetic.png", key: "aesthetic" },
-  { name: "Spiritual", icon: "/categories/spiritual2.png", key: "spiritual" },
-  { name: "Lifestyle", icon: "/categories/lifestyle.png", key: "lifestyle" },
+  { name: "Entertainment", icon: "/categories/entertainment.png", key: "entertainment", color: "c1" },
+  { name: "Superheroes", icon: "/categories/superheroes.png", key: "marvel_dc", color: "c2" },
+  { name: "Sports", icon: "/categories/sports.png", key: "sports", color: "c3" },
+  { name: "Anime", icon: "/categories/anime.png", key: "anime", color: "c4" },
+  { name: "Aesthetic", icon: "/categories/aesthetic.png", key: "aesthetic", color: "c5" },
+  { name: "Spiritual", icon: "/categories/spiritual2.png", key: "spiritual", color: "c6" },
+  { name: "Lifestyle", icon: "/categories/lifestyle.png", key: "lifestyle", color: "c7" },
 ];
 
 export default function CategorySection() {
@@ -36,24 +36,46 @@ export default function CategorySection() {
         {/* FIRST ROW */}
         <div className="categories-row row-top">
           {firstRow.map((cat,index)=>(
+
             <div className="category-item" key={index}>
-              <div className="category-circle">
-                <img src={cat.icon} alt={cat.name} className="category-icon"/>
+
+              <div className={`category-circle ${cat.color}`}>
+
+                <img
+                  src={cat.icon}
+                  alt={cat.name}
+                  className="category-icon"
+                />
+
               </div>
+
               <p className="category-name">{cat.name}</p>
+
             </div>
+
           ))}
         </div>
 
         {/* SECOND ROW */}
         <div className="categories-row row-bottom">
           {secondRow.map((cat,index)=>(
+
             <div className="category-item" key={index}>
-              <div className="category-circle">
-                <img src={cat.icon} alt={cat.name} className="category-icon"/>
+
+              <div className={`category-circle ${cat.color}`}>
+
+                <img
+                  src={cat.icon}
+                  alt={cat.name}
+                  className="category-icon"
+                />
+
               </div>
+
               <p className="category-name">{cat.name}</p>
+
             </div>
+
           ))}
         </div>
 
