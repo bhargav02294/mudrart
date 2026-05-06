1de8e1a4e346d261cf36171ce2940099295b8959
bhargav02294 <bhargavahire22@gmail.com>
2026-05-06 14:47:27 +0530

    123456789

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -21,6 +21,7 @@ import "./styles/cart.css";
 import "./styles/navbar.css";
 import "./styles/footer.css";
 import "./styles/posterCard.css";
+import "./styles/digital.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
--- client/src/pages/DigitalCheckout.jsx
+++ client/src/pages/DigitalCheckout.jsx
@@ -1,6 +1,8 @@
 import {useState,useEffect} from "react"
 import {useParams,useNavigate} from "react-router-dom"
 import Navbar from "../components/Navbar"
+import "../styles/digital.css";
+
 
 export default function DigitalCheckout(){
 
