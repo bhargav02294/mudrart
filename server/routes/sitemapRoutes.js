17b3d708bcf0c2122c68f257865d0ecb683ffa1a
bhargav02294 <bhargavahire22@gmail.com>
2026-05-11 08:37:45 +0530

    shah rukh kh

--- server/routes/sitemapRoutes.js
+++ server/routes/sitemapRoutes.js
@@ -119,21 +119,15 @@ router.get("/sitemap.xml", async (req, res) => {
        FINAL XML
     ====================================== */
 
-    const xml = `
+    const xml = `<?xml version="1.0" encoding="UTF-8"?>
 
-      <?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 
-      <urlset
-        xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
-      >
+${staticXml}
 
-        ${staticXml}
+${productXml}
 
-        ${productXml}
-
-      </urlset>
-
-    `;
+</urlset>`;
 
     /* ======================================
        RESPONSE
